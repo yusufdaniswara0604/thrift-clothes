dab98132a04aeb3d63c606506da427dc5d3bb0ef
yusufdaniswara0604 <yusufdani9@gmail.com>
2024-06-12 13:57:09 +0700

    tambah template flowbite

---
+++ resources/views/template.blade.php
@@ -0,0 +1,16 @@
+<!doctype html>
+<html>
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  @vite('resources/css/app.css')
+  <link href="https://cdnjs.cloudflare.com/ajax/libs/flowbite/2.3.0/flowbite.min.css"  rel="stylesheet" />
+</head>
+<body>
+  
+<!-- KODINGAN KALIAN DI SINI -->
+
+</body>
+@vite('resources/js/app.js')
+<script src="https://unpkg.com/flowbite@latest/dist/flowbite.min.js"></script>
+</html>
--- routes/web.php
+++ routes/web.php
@@ -6,3 +6,6 @@ Route::get('/', function () {
     return view('welcome');
 });
 
+Route::get('/template', function () {
+    return view('template');
+});
