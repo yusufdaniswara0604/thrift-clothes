8a5bf08f9d89fb630a462169e602a2575b0c3599
yusufdaniswara0604 <yusufdani9@gmail.com>
2024-06-19 17:37:50 +0700

    membuat halaman produk dan menambahkan gambar

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Support\Facades\Route;
 
+
 Route::get('/', function () {
     return view('welcome');
 });
@@ -9,3 +10,9 @@ Route::get('/', function () {
 Route::get('/template', function () {
     return view('template');
 });
+
+Route::get('/katalog', function () {
+    return view('katalog');
+});
+
+
